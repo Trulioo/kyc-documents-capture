cdb608c55dfdc50ed43332229d6177008ef5de15
Trulioo Release Automation <release-automation@trulioo.com>
2026-06-11 16:52:52 -0700

    Release 3.1.0

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         .binaryTarget(
             name: "TruliooKYCDocumentsCapture",
             url: "https://github.com/Trulioo/kyc-documents-capture/releases/download/3.1.0/TruliooKYCDocumentsCapture.xcframework.zip",
-            checksum: "db7c526b7f9f790ce4f3a9693d2494377b986cb43d27308c958adeb3e14b282f"
+            checksum: "b34a8c51b27d8a258a31011279f6b2357e559095887683c561074425a590147d"
         ),
         .binaryTarget(
             name: "TruliooKYCDocumentsCaptureRuntime",
             url: "https://github.com/Trulioo/kyc-documents-capture/releases/download/3.1.0/TruliooKYCDocumentsCaptureRuntime.xcframework.zip",
-            checksum: "22a315139cc8faef4cd2a23418704714ebf0b01c307745cf24cf349f2b185cb2"
+            checksum: "0fc89b3b290645216cfced42176dd9db4f78dfd6b104d84eb1d5840570772862"
         ),
         .target(
             name: "TruliooKYCDocumentsCaptureDependencies",
